fix backpropagation error

examples/story_diffusion/ad/data/dataset.py
@@ -95,7 +95,7 @@ def _build_transform():
     std = np.array([0.26862954, 0.26130258, 0.27577711]) * 255
     transforms = albumentations.Compose(
         [
-            albumentations.Resize((224, 224), interpolation=cv2.INTER_CUBIC),
+            albumentations.Resize(224, 224, interpolation=cv2.INTER_CUBIC),
             albumentations.Normalize(mean.tolist(), std.tolist()),
         ]
     )
@@ -266,7 +266,9 @@ class TextVideoDataset:
             pixel_values = pixel_values[0]
         if self.return_start_end_frames:
             conditions = np.transpose(pixel_values[[0, -1]], (0, 2, 3, 1))  # (2 c h w) -> (2 h w c)
-            conditions = self.clip_transforms(conditions)  # (2 h w c) -> (2, 224, 224, 3)
+            conditions = np.stack(
+                [self.clip_transforms(image=img)["image"] for img in conditions]
+            )  # (2 h w c) -> (2, 224, 224, 3)
             conditions = np.transpose(conditions, (0, 3, 1, 2))  # (2, 224, 224, 3) -> (2, 3, 224, 224)
         pixel_values = (pixel_values / 127.5 - 1.0).astype(np.float32)
 

examples/story_diffusion/ad/modules/diffusionmodules/semantic_motion_predictor.py
@@ -45,7 +45,10 @@ class SemanticMotionPredictor(nn.Cell):
         # interpolate the image embedding to the target length
         # Bs, F, D = x.shape
         x = x @ self.proj_in
-        x = ops.interpolate(x.transpose(0, 2, 1), size=target_len, mode="linear").transpose(0, 2, 1)
+        # x = ops.interpolate(x.transpose(0, 2, 1), size=target_len, mode="linear").transpose(0, 2, 1)
+        assert x.shape[1] == 2, f"Expect to receive two frames as conditions, but got shape {x.shape}"
+        start, end = x[:, 0, :], x[:, 1, :]
+        x = ops.stack([start + (end - start) * t for t in ms.numpy.linspace(0, 1, target_len)], axis=1)
         x = x + self.positional_embedding
 
         x = self.ln_pre(x)
@@ -55,7 +58,5 @@ class SemanticMotionPredictor(nn.Cell):
         x = x.permute(1, 0, 2)  # LND -> NLD
 
         x = self.ln_post(x)
-
-        if self.proj_out is not None:
-            x = x @ self.proj_out
+        x = x @ self.proj_out
         return x

examples/story_diffusion/ad/modules/diffusionmodules/unet3d.py
@@ -580,7 +580,7 @@ class UNet3DModel(nn.Cell):
         # append openclip image embedding to the text embedding
         if append_to_context is not None:
             append_to_context = append_to_context.reshape(B * F, -1, append_to_context.shape[-1])  # (b*f, l, dim_emb)
-            context = ops.cat([context, append_to_context], axis=1)  # (b*f, 77+l, dim_emb)
+            context = ops.cat([context, append_to_context.to(context.dtype)], axis=1)  # (b*f, 77+l, dim_emb)
 
         h = x
 