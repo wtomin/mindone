0ecb276c5120d7a8066aa4b1bbc55f64422606a3
Didan Deng <33117903+wtomin@users.noreply.github.com>
2024-10-16 10:10:19 +0800

    allow use_fp16

--- examples/story_diffusion/ad/modules/diffusionmodules/semantic_motion_predictor.py
+++ examples/story_diffusion/ad/modules/diffusionmodules/semantic_motion_predictor.py
@@ -2,9 +2,7 @@ from ad.modules.encoders._common import LayerNorm
 from ad.modules.encoders.image_encoder import Transformer
 
 import mindspore as ms
-from mindspore import Parameter, Tensor
-from mindspore import dtype as mstype
-from mindspore import nn, ops
+from mindspore import Parameter, Tensor, nn, ops
 from mindspore.common.initializer import TruncatedNormal, initializer
 
 
@@ -23,10 +21,10 @@ class SemanticMotionPredictor(nn.Cell):
         output_dim: int = None,
         epsilon: float = 1e-5,
         use_quick_gelu: bool = False,
-        dtype: mstype = ms.float32,
+        use_fp16: bool = False,
     ):
         super().__init__()
-        self.dtype = dtype
+        self.dtype = ms.float16 if use_fp16 else ms.float32
         self.ln_pre = LayerNorm((width,), epsilon=epsilon)
         self.num_frames = num_frames
         self.positional_embedding = Parameter(initializer(TruncatedNormal(0.01), [num_frames, width], dtype=self.dtype))
@@ -35,7 +33,7 @@ class SemanticMotionPredictor(nn.Cell):
             width, layers, heads, epsilon=epsilon, use_quick_gelu=use_quick_gelu, dtype=self.dtype
         )
         scale = width**-0.5
-        self.ln_post = LayerNorm((width,), epsilon=epsilon)
+        self.ln_post = LayerNorm((width,), epsilon=epsilon).to_float(self.dtype)
         output_dim = output_dim if output_dim is not None else embed_dim
         self.proj_out = Parameter(scale * ms.numpy.randn(width, output_dim, dtype=self.dtype))
         self.proj_in = Parameter(scale * ms.numpy.randn(embed_dim, width, dtype=self.dtype))
