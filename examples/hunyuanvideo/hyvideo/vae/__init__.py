2d187a55f717391e1716dfc1f4936cfab81da52b
Didan Deng <33117903+wtomin@users.noreply.github.com>
2025-01-16 09:54:53 +0800

    allow set attention mode = flash (default)

--- examples/hunyuanvideo/hyvideo/vae/__init__.py
+++ examples/hunyuanvideo/hyvideo/vae/__init__.py
@@ -52,7 +52,5 @@ def load_vae(
     vae.set_train(False)
     for param in vae.trainable_params():
         param.requires_grad = False
-    if logger is not None:
-        logger.info(f"VAE to dtype: {vae.dtype}")
 
     return vae, vae_path, spatial_compression_ratio, time_compression_ratio
