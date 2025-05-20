faf84057bd0eea2a4dcb5c51f532ec080ba562be
Didan Deng <33117903+wtomin@users.noreply.github.com>
2025-05-20 18:31:08 +0800

    fix docs

--- mindone/transformers/models/cohere/modeling_cohere.py
+++ mindone/transformers/models/cohere/modeling_cohere.py
@@ -226,7 +226,7 @@ class CohereAttention(nn.Cell):
         if self.config._attn_implementation != "eager":
             if self.config._attn_implementation == "sdpa" and kwargs.get("output_attentions", False):
                 logger.warning_once(
-                    "`torch.nn.functional.scaled_dot_product_attention` does not support `output_attentions=True`. Falling back to "
+                    "`scaled_dot_product_attention` does not support `output_attentions=True`. Falling back to "
                     'eager attention. This warning can be removed using the argument `attn_implementation="eager"` when loading the model.'
                 )
             else:
