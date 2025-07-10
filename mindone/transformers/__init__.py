1fe745f72a1ab666241df6c6fa0f56f172e86241
Didan Deng <33117903+wtomin@users.noreply.github.com>
2025-07-10 11:41:52 +0800

    import modules

--- mindone/transformers/__init__.py
+++ mindone/transformers/__init__.py
@@ -185,6 +185,7 @@ from .models.imagegpt import (
     ImageGPTModel,
     ImageGPTPreTrainedModel,
 )
+from .models.jamba import JambaForCausalLM, JambaForSequenceClassification, JambaModel, JambaPreTrainedModel
 from .models.levit import (
     LevitFeatureExtractor,
     LevitForImageClassification,
