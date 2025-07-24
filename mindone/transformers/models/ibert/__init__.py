4ba5ad69a78e2933d34ee952159840b5b0bb8bf7
Didan Deng <33117903+wtomin@users.noreply.github.com>
2025-07-24 21:13:23 +0800

    init

---
+++ mindone/transformers/models/ibert/__init__.py
@@ -0,0 +1,11 @@
+from .modeling_ibert import (
+    IBertForMaskedLM,
+    IBertForMultipleChoice,
+    IBertForQuestionAnswering,
+    IBertForSequenceClassification,
+    IBertForTokenClassification,
+    IBertModel,
+    IBertPreTrainedModel,
+)
+
+from transformers import IBertConfig
