27180586f34dd6a198bdb2840cb9c5fcdb23387e
Didan Deng <33117903+wtomin@users.noreply.github.com>
2025-08-07 12:00:07 +0800

    fix FLUX to(model_dtype) error

--- examples/x-flux/src/flux/util.py
+++ examples/x-flux/src/flux/util.py
@@ -286,7 +286,7 @@ def load_flow_model(name: str, hf_download: bool = True):
     ):
         ckpt_path = hf_hub_download(configs[name].repo_id, configs[name].repo_flow)
 
-    model = Flux(configs[name].params).to(mindspore.bfloat16)
+    model = Flux(configs[name].params).to_float(mindspore.bfloat16)
 
     if ckpt_path is not None:
         print("Loading checkpoint")
