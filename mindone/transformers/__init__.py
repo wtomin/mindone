b73af9b17117d32c616e5b4178c8b445c8e2237d
Didan Deng <33117903+wtomin@users.noreply.github.com>
2025-08-12 14:34:15 +0800

    model init

--- mindone/transformers/__init__.py
+++ mindone/transformers/__init__.py
@@ -289,6 +289,14 @@ from .models.gemma2 import (
     Gemma2PreTrainedModel,
 )
 from .models.gemma3 import Gemma3ForCausalLM, Gemma3ForConditionalGeneration, Gemma3PreTrainedModel, Gemma3TextModel
+from .models.gemma3n import (
+    Gemma3nAudioEncoder,
+    Gemma3nForCausalLM,
+    Gemma3nForConditionalGeneration,
+    Gemma3nModel,
+    Gemma3nPreTrainedModel,
+    Gemma3nTextModel,
+)
 from .models.glm import (
     GlmForCausalLM,
     GlmForSequenceClassification,
---
+++ mindone/transformers/models/gemma3n/__init__.py
@@ -0,0 +1,8 @@
+from .modeling_gemma3n import (
+    Gemma3nAudioEncoder,
+    Gemma3nForCausalLM,
+    Gemma3nForConditionalGeneration,
+    Gemma3nModel,
+    Gemma3nPreTrainedModel,
+    Gemma3nTextModel,
+)
